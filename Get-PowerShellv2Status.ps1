6fc333c0cf7a7ffd08fb15a917117943c8114d93
snajd <robin.engstrom@gmail.com>
2018-03-02 21:50:26 +0100

    ny fil

---
+++ Get-PowerShellv2Status.ps1
@@ -0,0 +1,8 @@
+# check if PowerShell Version 2 is installed (may be used for evasion of logging and Language Mode restrictions)
+
+if ((Get-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2).State -eq "Disabled") {
+    return $true
+}
+else {
+    return $false
+}
